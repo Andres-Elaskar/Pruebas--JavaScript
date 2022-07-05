990b4b5bc95dd2fdc34a828b7626953aedda4de8
Andy Elaskar <andres_elaskar@hotmail.com>
2022-07-05 13:15:49 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+prompt ('Desafio opcional')
+
+//let ingresarNumero = parseInt(prompt('ingresar numero'));
+//for (let i = 1; i <= 100; i ++); {
+    //if( i ==9) {
+    
+        //continue;
+   //}
+   // let resultado = ingrasarNumero *i ;
+   // alert (ingrsarNumero +'x' + i +' + resultado');
+
+//while (entrada != 'esc')
+
+for (let i = 1; i <= 20; i ++) {
+    let ingresarNombre = prompt ('Ingresar nombre')
+    alert ('Turno Nro '+i+'Nombre: '+ingresarNombre);
+}
